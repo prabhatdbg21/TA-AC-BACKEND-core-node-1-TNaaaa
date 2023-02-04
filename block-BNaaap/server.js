9f411e47cd0ea9860f351997284df4010a51c3ba
Prabhat <prabhatdbg21@gmail.com>
2023-02-04 17:36:40 +0530

    complited

--- block-BNaaap/server.js
+++ block-BNaaap/server.js
@@ -3,6 +3,7 @@ var http = require ('http');
 var server = http.createServer(handleRequest);
 
 function handleRequest (req, res) {
+    console.log(req.method);
     // handle request
     res.writeHead(201, {'Content-Type': 'text/html'});
     res.write('<h1> Prabhat </h1>');
