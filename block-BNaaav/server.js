d3cfb5cef60dd291886ea5aa94c2b49309e1fbde
Prabhat <prabhatdbg21@gmail.com>
2023-02-11 11:42:02 +0530

    from Q2 not solved

---
+++ block-BNaaav/server.js
@@ -0,0 +1,23 @@
+var http = require ('http');
+var url = require('url');
+var fs = require('fs');
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+    var parsedUrl = url.parse(req.url);
+    var pathname = parsedUrl.pathname;
+
+    if(req.method === 'GET' && pathname === '/') {
+        res.setHeader('Content-Type' , 'text/html'); 
+        fs.createReadStream('./index.html').pipe(res);
+    } else if(req.method === 'GET' && pathname === '/about') {
+        res.setHeader('Content-Type' , 'text/html'); 
+        fs.createReadStream('./about.html').pipe(res);
+    } else {
+        res.writeHead(404, {'Content-Type': "text/html"}); 
+        res.end('<h2>Page not Found</h2>');
+    }
+}
+
+server.listen(3001)
